e06939b8ec248448470cc5a8f62f3874b4dc6e90
jinkshower <askthem@naver.com>
2024-01-31 21:54:22 +0900

    feat: 로그인 기능 구현
    commonresponse null값 반환하지 않게 설정

--- src/main/java/com/spring/todomanagement/web/CommonResponse.java
+++ src/main/java/com/spring/todomanagement/web/CommonResponse.java
@@ -1,10 +1,12 @@
 package com.spring.todomanagement.web;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CommonResponse<T> {
     private int statusCode;
     private String message;
--- src/main/java/com/spring/todomanagement/web/UserController.java
+++ src/main/java/com/spring/todomanagement/web/UserController.java
@@ -2,28 +2,36 @@ package com.spring.todomanagement.web;
 
 import com.spring.todomanagement.service.user.UserService;
 import com.spring.todomanagement.web.dto.AuthRequestDto;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RequiredArgsConstructor
-@Controller
+@RestController
 public class UserController {
 
     private final UserService userService;
 
     @PostMapping("/user/signup")
-    public ResponseEntity<CommonResponse> signup(@RequestBody AuthRequestDto requestDto, RedirectAttributes redirectAttributes) {
+    public ResponseEntity<CommonResponse> signup(@RequestBody AuthRequestDto requestDto) {
         userService.signup(requestDto);
+        log.info("회원등록");
         return ResponseEntity.ok().body(CommonResponse.builder()
                         .statusCode(HttpStatus.CREATED.value())
                         .message("회원등록 되었습니다.").build());
+    }
+
+    @PostMapping("/user/login")
+    public ResponseEntity<CommonResponse> login(@RequestBody AuthRequestDto requestDto, HttpServletResponse response) {
+        userService.login(requestDto, response);
+        return ResponseEntity.ok().body(CommonResponse.builder()
+                .statusCode(HttpStatus.OK.value())
+                .message("로그인 되었습니다.").build());
     }
 }
