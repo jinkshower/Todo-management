7ece58540f5fa023c6e1413c979b21cda5a748af
jinkshower <askthem@naver.com>
2024-02-19 15:53:18 +0900

    example test

---
+++ src/test/java/com/spring/todomanagement/ExampleTest.java
@@ -0,0 +1,13 @@
+package com.spring.todomanagement;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+
+    @Test
+    void test() {
+        assertTrue(true);
+    }
+}
