e4a47234bac4a4d7aabd9d375cefeff92a18e601
jinkshower <askthem@naver.com>
2024-02-19 17:43:32 +0900

    feat: UserFixture 생성

---
+++ src/test/java/com/spring/todomanagement/common/UserFixture.java
@@ -0,0 +1,22 @@
+package com.spring.todomanagement.common;
+
+import com.spring.todomanagement.todo_mangement.domain.User;
+
+public interface UserFixture {
+
+    String ANOTHER_PREFIX = "another-";
+    Long TEST_USER_ID = 1L;
+    Long TEST_ANOTHER_USER_ID = 2L;
+    String TEST_USER_NAME = "username";
+    String TEST_USER_PASSWORD = "12345678";
+
+    User TEST_USER = User.builder()
+        .name(TEST_USER_NAME)
+        .password(TEST_USER_PASSWORD)
+        .build();
+
+    User TEST_ANOTHER_USER = User.builder()
+        .name(ANOTHER_PREFIX + TEST_USER_NAME)
+        .password(TEST_USER_PASSWORD)
+        .build();
+}
