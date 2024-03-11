ae80c87869a4f8314cebdafd585533e2a640e905
jinkshower <askthem@naver.com>
2024-03-11 21:53:38 +0900

    feat: page 정보 dto 생성

---
+++ src/main/java/com/spring/todomanagement/todo_mangement/dto/PageDto.java
@@ -0,0 +1,27 @@
+package com.spring.todomanagement.todo_mangement.dto;
+
+import java.util.Objects;
+import lombok.Getter;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+@Getter
+public class PageDto {
+
+    private int currentPage;
+    private int size;
+    private String sortBy;
+
+    public Pageable toPageable() {
+        if (Objects.isNull(sortBy)) {
+            return PageRequest.of(currentPage - 1, size);
+        } else {
+            return PageRequest.of(currentPage - 1, size, Sort.by(sortBy).descending());
+        }
+    }
+
+    public Pageable toPageable(String sortBy) {
+        return PageRequest.of(currentPage - 1, size, Sort.by(sortBy).descending());
+    }
+}
